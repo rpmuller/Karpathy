78be9038213a055c6839244c2ecc1c9f4c5eaf41
Rick Muller <rpmuller@gmail.com>
2024-05-24 19:39:27 -0400

    Updated and finished the doggo mnist exercise.

--- flux-mnist.jl
+++ flux-mnist.jl
@@ -2,10 +2,9 @@
 #  video at: https://www.youtube.com/watch?v=zmlulaxatRs
 
 using Flux, Images, MLDatasets
-
 using Flux: crossentropy, onecold, onehotbatch, train!, params
-
 using LinearAlgebra, Random, Statistics
+using Plots
 
 # set random seed
 
@@ -38,12 +37,12 @@ loss(x,y) = crossentropy(model(x), y)
 
 ps = params(model) # initialize all parameters
 
-learning_rate = 0.05
+learning_rate = 0.01
 opt = ADAM(learning_rate)
 
 loss_history = []
 
-epochs = 100
+epochs = 500
 
 for epoch in 1:epochs
     train!(loss, ps, [(X_train, y_train)], opt)
@@ -52,4 +51,27 @@ for epoch in 1:epochs
     println("Epoch = $epoch: Training Loss = $train_loss")
 end
 
-# at 16:26 in video
+# make predictions
+y_hat_raw = model(X_test)
+y_hat = onecold(y_hat_raw) .- 1
+y = y_test_raw
+mean(y_hat .== y) # doggo got 96.24%
+# CNN gets 99.83% according to wikipedia
+
+# display results
+check = [y_hat[i] == y[i] for i in 1:length(y)]
+index = collect(1:length(y))
+check_display = [index y_hat y check]
+vscodedisplay(check_display)
+
+misclass_index=9
+
+img = X_test_raw[:,:,misclass_index]
+
+colorview(Gray, img')
+y[misclass_index]
+y_hat[misclass_index]
+
+gr(size = (600,600))
+pl = plot(1:epochs, loss_history, xlabel="Epochs", ylabel="Loss", title="Learning Curve", legend=false,
+    color=:blue, linewidth=2)
