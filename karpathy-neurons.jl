f90aa479a268082a21639febfbb585c67a2084c5
Rick Muller <rpmuller@gmail.com>
2024-05-25 16:08:30 -0400

    Added call to tanh and streamlined calling neurons

--- karpathy-neurons.jl
+++ karpathy-neurons.jl
@@ -4,37 +4,32 @@ using Flux
 
 # I think this is essentially the same network that AK uses in micrograd:
 model = Dense(2,1,tanh)
-
 out = model([2,0])
-out
-
 
 
 # We're going to use our Value object from before to build something similar to this:
 include("Value.jl")
 struct Neuron
-    nin::Int64
     w::Vector{Value}
     b::Value
 end
-function Neuron(n)
+function Neuron(n::Int64)
     rando() = 2*rand(Float64)-1
     w = [Value(rando(),"w$i") for i in 1:n]
     b = Value(rando(),"b")
-    return Neuron(n,w,b)
+    return Neuron(w,b)
     print(b)
 end
 
-function (neur::Neuron)(x::Vector{Value})
-    return transpose(neur.w)*x+neur.b
-end
-function (neur::Neuron)(x::Vector{Float64})
-    return sum([neur.w[i]*Value(x[i],"x$i") for i in 1:length(x)]) + neur.b
-end
+# Call neuron object as a function:
+call(neur::Neuron,x::Vector{Value}) = tanh(transpose(neur.w)*x+neur.b)
+call(neur::Neuron,x::Vector{Float64}) = call(neur,[Value(x[i],"x$i") for i in 1:length(x)])
+(neur::Neuron)(x::Vector{Value}) = call(neur,x)
+(neur::Neuron)(x::Vector{Float64}) = call(neur,[Value(x[i],"x$i") for i in 1:length(x)])
 
 o = Neuron(2)
 
+# These are all the same:
 o([Value(1.),Value(2.)])
-o.w
-transpose(o.w)*[Value(1),Value(2)]
 o([1.,2.])
+call(o,[1.,2.])
