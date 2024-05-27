223889b744a77821f4fd22d9cb34fcaa410d158f
Rick Muller <rpmuller@gmail.com>
2024-05-27 09:54:22 -0400

    Updating files from first makemore lesson

---
+++ karpathy-makemore1-nn.jl
@@ -0,0 +1,63 @@
+# Currently at 1:04 in the vid
+# Doing the same work as karpathy-makemore1.jl
+# but using neural nets.
+using Plots
+using Flux
+using Flux: onehot,onehotbatch
+# For future reference, here's how to call onehot:
+#onehot(7,1:10)
+
+xs, ys = [], []
+for word in words[1:1]
+    chs = vcat('.',collect(word),'.')
+    for (ch1,ch2) in zip(chs,chs[2:end])
+        ix1,ix2 = stoi[ch1],stoi[ch2]
+        push!(xs,ix1)
+        push!(ys,ix2)
+    end
+end
+
+# This is creating the transpose of what AK has.
+# I think because of the vertical vs horiz vector
+# confusion in Julia.
+# Can put a transpose in here if desired:
+xenc = onehotbatch(xs,1:27)'
+#heatmap(xenc)
+
+# Flux uses bools for onehot encoding.
+# Doesn't seem like I have to worry about it:
+W = randn(27,27)
+logits = xenc*W # logits = log-counts:
+
+# Need to get outputs as probabilities, which should be 
+# positive and sum to one. Interpret these outputs as 
+# log-counts. We want to exponentiate and then normalize
+# these to get probs. This is called softmax.
+
+probs = exp.(logits)
+for i in 1:size(probs)[1]
+    probs[i,:] /= sum(probs[i,:])
+end
+
+
+# at 1:29 looking in detail at one example .emma.
+nlls = zeros(Float64,5)
+for i in 1:5
+    x = xs[i]
+    y = ys[i]
+    println("--------")
+    println("bigram example $i: $(itos[i]) $(itos[y]) indices $x, $y")
+    println("input to the neural net: $x")
+    println("output probabilities from neural net: $probs")
+    println("label of actual next character: $y")
+    p = probs[i,y]
+    println("probability assigned by the net to the next char: $p")
+    logp = log(p)
+    println("log likelihood $logp")
+    nll = -logp
+    println("negative log likelihood: $nll")
+    nlls[i] = nll
+end
+println("========")
+println("average neg log likelihood, i.e. loss: $(mean(nll))")
+
---
+++ karpathy-makemore1.jl
@@ -0,0 +1,73 @@
+
+using Plots
+using StatsBase
+
+# Read names.txt into words:
+f = open("names.txt","r")
+    s = read(f,String)
+close(f)
+words = split(s,"\n")
+words[1:10]
+
+# Make character mapping arrays
+chars = ".abcdefghijklmnopqrstuvwxyz"
+stoi = Dict( s => i for (i,s) in enumerate(chars))
+itos = Dict( i => s for (i,s) in enumerate(chars))
+
+# Create the count array and fill it
+N = zeros(Int32,27,27)
+for word in words
+    chs = vcat('.',collect(word),'.')
+    for (ch1,ch2) in zip(chs,chs[2:end])
+        ix1,ix2 = stoi[ch1],stoi[ch2]
+        N[ix1,ix2] += 1
+    end
+end
+
+# Might be worth some time getting his plotting to work.
+# But not now.
+heatmap(N)
+
+# Example of multinomial sampling that AK goes through at 29:00:
+#histogram(sample(0:2,pweights([0.6064, 0.3033,0.0903]),10000))
+
+# Create a matrix of the letter probabilities
+nrows,ncols = size(P)
+P = float(N.+1) # The 1 here is the model smoothing
+for i in 1:nrows
+    P[i,:] /= sum(P[i,:])
+end
+
+# Sample the distribution of letters:
+for _ in 1:20
+    ix = 1
+    name = []
+    while true
+        row = N[ix,:]
+        p = P[ix,:]
+        ix = sample(1:27,pweights(p),1)[1]
+        if (ix==1) break end
+        push!(name,itos[ix])
+    end
+    println(join(name))
+end
+
+# Evaluate the quality of the bigram model we've constructed.
+# Use avg neg log likelihood
+log_likelihood = 0
+n = 0
+for word in words
+    chs = vcat('.',collect(word),'.')
+    for (ch1,ch2) in zip(chs,chs[2:end])
+        ix1,ix2 = stoi[ch1],stoi[ch2]
+        log_likelihood += log(P[ix1,ix2])
+        n += 1
+    end
+end    
+nll = -log_likelihood
+nll/n # average negative log likelihood, which is what we'll use as the loss
+# we will also add a smoothing to all numbers to soften the low probs
+
+# Since the next part of the video uses neural nets to do the same
+# thing, I'm going to move this into another file.
+# Currently at 1:04, and I'm moving to karpathy-makemore1-nn.jl
