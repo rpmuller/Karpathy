0cb0bed5749b193558a4751fdf64af3b5db109f8
Rick Muller <rpmuller@gmail.com>
2024-06-11 06:27:22 -0400

    Some actual progress. Softmax now works, and I'm now summing my softmax function along the right direction, so the two match. My labeled Y data needs a onehot pass, I think.

--- makemore-mlp2.jl
+++ makemore-mlp2.jl
@@ -8,7 +8,7 @@
 # "now made respectable" comment
 
 using Flux
-using Flux: train!, params, gradient, crossentropy, softmax, DataLoader
+using Flux: train!, params, gradient, crossentropy, softmax, DataLoader, update!
 using Statistics
 
 # hyperparameters
@@ -16,7 +16,7 @@ embedding_depth = 2    # dimension of the character embedding
 block_size = 3         # context length: how many chars to we use to predict next one?
 
 # Read names.txt into words array:
-words = split(read("names.txt",String),"\n")
+words = split(read("names.txt",String),"\r\n")
 
 # Create character embeddings.
 chars = ".abcdefghijklmnopqrstuvwxyz"
@@ -42,7 +42,8 @@ function build_dataset(words)
 	for i in 1:nrows
     	X[i,:] = X0[i]
 	end
-	return X,Y' # note transpose
+	return X,Vector(Y) # note transpose
+	#return X,Y' # note transpose
 end
 
 # Forward pass
@@ -53,15 +54,16 @@ function predict(X,C,W1,b1,W2,b2)
 end
 
 # TODO: get cross entropy working below. Currently Y is the wrong shape.
-function mloss(X,Y)
-    logits = predict(X,C,W1,b1,W2,b2)
-    counts = exp.(logits)
-    prob = counts./sum(counts,dims=2)
-
-	loss = -mean(log.(prob[:,Y])) # negative log likelihood
-	#loss = crossentropy(logits,Y)
+function custom_crossentropy(logits,Y)
+    # These now match:
+    #prob = counts./sum(counts,dims=1)
+    prob = softmax(logits)
+	loss = -mean(log.([prob[i,Y[i]] for i in 1:length(Y)])) # negative log likelihood
+	#loss = crossentropy(logits,Y) # Doesn't give the same result
     return loss
 end
+
+mloss(X,Y) = custom_crossentropy(predict(X,C,W1,b1,W2,b2),Y)
 
 n1 = 8*length(words)÷10
 n2 = 9*length(words)÷10
@@ -73,11 +75,22 @@ Xsm,Ysm = build_dataset(words[1:100])
 C = randn(27,embedding_depth)  # Build embedding lookup table C.
 
 W1 = randn(6,100)
-b1 = randn(100)'
+b1 = randn(1,100)
 
 W2 = randn(100,27)
-b2 = randn(27)'
+b2 = randn(1,27)
 
+logits = predict(Xsm,C,W1,b1,W2,b2)
+#counts = exp.(logits)
+prob = counts./sum(counts,dims=1)
+prob2 = softmax(logits)
+loss = -mean(log.([prob[i,Ysm[i]] for i in 1:length(Ysm)])) # negative log likelihood
+loss2 = crossentropy(logits,Ysm) # Doesn't give the same result
+
+
+emb = C[Xsm,:]
+emb2 = reshape(emb,size(emb,1),6)
+emb2*W1.+b1
 
 ps = Flux.params(C,W1,b1,W2,b2)
 
@@ -88,16 +101,25 @@ loss_history = []
 
 epochs = 50
 
-# TODO: get code working with minibatched. Currently works with Xsm, but runs 
+# TODO: get code working with minibatch. Currently works with Xsm, but runs 
 # out of memory for larger inputs.
 Xin,Yin = Xsm,Ysm
 size(Xin)
 size(Yin)
-#data = [(Xin,Yin)] # no minibatches
-data = DataLoader((Xin,Yin),batchsize=50,partial=false)
+length(Yin)
+data = [(Xin,Yin)] # no minibatches
+#data = DataLoader((Xin,Yin),batchsize=50,partial=false)
 for epoch in 1:epochs
-    train!(mloss, ps, data, opt)
+    #train!(mloss, ps, data, opt)
+    grads = gradient(()->mloss(Xin,Yin),ps)
+    for p in ps
+        update!(p,-learning_rate * grads[p])
+    end
     train_loss = mloss(Xin, Yin)
     push!(loss_history, train_loss)
     println("Epoch = $epoch: Training Loss = $train_loss")
 end	
+
+f[1 2 3; 4 5 6] .+ vec([1,1])
+
+ones(2,1)
--- test_pluto.jl
+++ test_pluto.jl
@@ -4,3 +4,14 @@
 
 # There is a Pluto extension that I'm not
 # going to mess with now.
+
+a = ones(2,3)
+b = ones(1,3)
+a .+ b
+
+using Flux
+
+y_label = Flux.onehotbatch([0, 1, 2, 1, 0], 0:2)
+y_model = softmax(reshape(-7:7, 3, 5) .* 1f0)
+sum(y_model; dims=1)
+Flux.crossentropy(y_model, y_label)
