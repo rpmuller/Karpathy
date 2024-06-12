0e53585004ce9804fa0d6de043a2e91fa35b4800
Rick Muller <rpmuller@gmail.com>
2024-06-12 12:57:14 +0000

    Adding printing and analysis niceties.

--- makemore-mlp2.jl
+++ makemore-mlp2.jl
@@ -5,24 +5,25 @@
 # vocabulary of 17k words, implemented in a 30-dimensional space.
 
 # This is the second part of the file, after the 
-# "now made respectable" comment
+# "now made respectable" comment. Now also including changes from 
+# [makemore part 3 video](https://www.youtube.com/watch?v=P6sfmUTpUmc&t=503s)
 
 using Flux
 using Statistics
+using Plots
 
-# hyperparameters
-embedding_depth = 2    # dimension of the character embedding
-block_size = 3         # context length: how many chars to we use to predict next one?
 
 # Read names.txt into words array:
-words = split(read("names.txt",String),"\r\n")
+words = split(read("names.txt",String),"\n")
 
 # Create character embeddings.
 chars = ".abcdefghijklmnopqrstuvwxyz"
 stoi = Dict( s => i for (i,s) in enumerate(chars))
 itos = Dict( i => s for (i,s) in enumerate(chars))
+vocab_size = length(itos)
 
 # Compile dataset for neural net:
+block_size = 3         # context length: how many chars to we use to predict next one?
 function build_dataset(words)
 	X0 = []
 	Y::Array{Int64} = []
@@ -48,13 +49,13 @@ end
 # Forward pass
 function predict(X,C,W1,b1,W2,b2)
 	emb = C[X,:]
-	h = tanh.(reshape(emb,(size(emb,1),6))*W1 .+ b1)
+	h = tanh.(reshape(emb,(size(emb,1), n_embed*block_size))*W1 .+ b1)
     return h*W2 .+ b2
 end
 
 function mloss(X,Y)
 	logits = predict(X,C,W1,b1,W2,b2)
-	Yoh = Flux.onehotbatch(Y,1:27)'
+	Yoh = Flux.onehotbatch(Y,1:vocab_size)'
 	loss = Flux.logitcrossentropy(logits,Yoh,dims=2)
 	# alternately
 	#loss = Flux.logitcrossentropy(Flux.softmax(logits),Yoh,dims=2)
@@ -66,15 +67,19 @@ n2 = 9*length(words)÷10
 Xtr,Ytr = build_dataset(words[1:n1])
 Xdev,Ydev = build_dataset(words[n1:n2])
 Xte,Yte = build_dataset(words[n2:end])
-Xsm,Ysm = build_dataset(words[1:100])
+#Xsm,Ysm = build_dataset(words[1:100])
 
-C = randn(27,embedding_depth)  # Build embedding lookup table C.
+# hyperparameters
+n_embed = 10        # dimension of the character embedding
+n_hidden = 200     # neurons in the MLP hidden layer
 
-W1 = randn(6,100)
-b1 = randn(1,100)
+C = randn(vocab_size,n_embed)  # Build embedding lookup table C.
 
-W2 = randn(100,27)
-b2 = randn(1,27)
+W1 = randn(n_embed * block_size, n_hidden)
+b1 = randn(1,n_hidden)
+
+W2 = randn(n_hidden,vocab_size)
+b2 = randn(1,vocab_size)
 
 emb = C[Xsm,:]
 
@@ -82,19 +87,22 @@ ps = Flux.params(C,W1,b1,W2,b2)
 
 learning_rate = 0.01
 opt = ADAM(learning_rate)
-
 loss_history = []
 
-epochs = 50
-
-Xin,Yin = Xtr,Ytr
-# Manually do the batching
-bsize = 32
+epochs = 500
+batch_size = 32 # Manually do the batching
 
 for epoch in 1:epochs
-	ix = rand(1:length(Yin),bsize)
-    Flux.train!(mloss, ps, [(Xin[ix,:],Yin[ix])], opt)
-    train_loss = mloss(Xin, Yin)
+	ix = rand(1:length(Ytr),batch_size)
+    Flux.train!(mloss, ps, [(Xtr[ix,:],Ytr[ix])], opt)
+    train_loss = mloss(Xtr[ix,:], Ytr[ix])
     push!(loss_history, train_loss)
     println("Epoch = $epoch: Training Loss = $train_loss")
 end	
+
+# Evaluate convergence and losses:
+plot(loss_history)
+mloss(Xtr,Ytr)
+mloss(Xdev,Ydev)
+mloss(Xte,Yte)
+
